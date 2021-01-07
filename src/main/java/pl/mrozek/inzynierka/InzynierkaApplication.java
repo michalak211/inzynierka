901017533ab85b7cb1a39bf91086ddbed0de150f
michalak211 <michalak211@gmail.com>
2021-01-07 19:31:53 +0100

    Initial commit

---
+++ src/main/java/pl/mrozek/inzynierka/InzynierkaApplication.java
@@ -0,0 +1,14 @@
+package pl.mrozek.inzynierka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InzynierkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InzynierkaApplication.class, args);
+        System.out.println("jest?");
+    }
+
+}
---
+++ src/test/java/pl/mrozek/inzynierka/InzynierkaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mrozek.inzynierka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InzynierkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
